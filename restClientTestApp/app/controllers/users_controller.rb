e2c56934e43889b6e22eab09848a7fed8d15b427
Omar S <osanseviero@gmail.com>
2015-07-31 23:47:02 -0500

    Added get request files and scripts

--- restClientTestApp/app/controllers/users_controller.rb
+++ restClientTestApp/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+	def index
+		@name = "Index action"
+	end
+
+	def show
+		@name = "Show action"
+	end
+
+	def new
+		@name = "New action"
+	end
+
+	def edit
+		@name = "Edit action"
+	end
+
+
 end
