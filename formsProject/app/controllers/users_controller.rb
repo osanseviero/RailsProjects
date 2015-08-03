1975ab63eb6eec5358ce4a2b9d4785fb51575c59
Omar S <osanseviero@gmail.com>
2015-08-02 19:19:12 -0500

    Added profile (no code yet, but now register form redirects to profile)

--- formsProject/app/controllers/users_controller.rb
+++ formsProject/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 
 		if @user.save
-			redirect_to new_user_path
+			redirect_to @user
 		else
 			render :new
 		end
@@ -22,11 +22,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if 
       @user.update(user_params)
-      redirect_to edit_user_path(@user)
+      redirect_to @user
     else
       render :edit
     end
    end
+
+ 	def show
+ 		@user = User.find(params[:id])
+ 	end
 
 
 	private
--- formsProject/config/routes.rb
+++ formsProject/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users, :only => [:new, :create, :edit, :update]
+  resources :users, :only => [:new, :create, :edit, :update, :show]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
