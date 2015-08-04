d04ac23fa2e557908a4a1bc543b7ea255a473384
Omar S <osanseviero@gmail.com>
2015-08-04 10:56:53 -0500

    Fixes and session controller

--- AuthenticationApp/config/routes.rb
+++ AuthenticationApp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'signin' => 'sessions#new'
+  get '/signin' => 'sessions#new'
   get '/signout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
