cba9e29ea3b006b4c9d85dc884b24ef393912347
Omar S <osanseviero@gmail.com>
2015-08-05 10:57:32 -0500

    Added index view and posts controller

--- AuthenticationApp/app/controllers/post_controller.rb
+++ AuthenticationApp/app/controllers/post_controller.rb
@@ -2,12 +2,18 @@ class PostController < ApplicationController
 	before_action :signed_in_user, only: [:new, :create]
 
 	def index
+		@posts = Post.all
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create 
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		@post.save
+		redirect_to root_path
 	end
 
 	def signed_in_user
--- AuthenticationApp/app/models/user.rb
+++ AuthenticationApp/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts
 	before_create :create_remember_token
 
   has_secure_password
