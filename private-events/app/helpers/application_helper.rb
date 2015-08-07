e7e24ac3998a4ee18b9640c6becff82cd125a0ba
Omar S <osanseviero@gmail.com>
2015-08-07 12:37:40 -0500

    Added registering and really simple login system

--- private-events/app/helpers/application_helper.rb
+++ private-events/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def current_user
+		remember_token = User.digest(cookies[:remember_token])
+    	@current_user ||= User.find_by(remember_token: remember_token)
+	end
 end
--- private-events/config/routes.rb
+++ private-events/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :users, only: ['new','create','show', 'index']
+  get 'signup' => 'users#new'
+
+  resources :sessions, only: ['new', 'create', 'destroy']
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
